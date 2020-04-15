b5a4db44aaaad5b3276a5838b059756490195fad
Jacob Carver <jake10693@gmail.com>
2020-04-15 14:22:23 -0700

    fully functioning finished application

--- script.js
+++ script.js
@@ -7,34 +7,65 @@ $(".saveBtn").click(function(event){
 })
 
 $(document).ready(function(){
-  $('#9').val(localStorage.getItem('  9AM'))
+  $('#9a').val(localStorage.getItem('  9AM'))
   console.log(localStorage.getItem('  9AM'));
 })
 $(document).ready(function(){
-  $('#10').val(localStorage.getItem('10AM'))
+  $('#10a').val(localStorage.getItem('10AM'))
   console.log(localStorage.getItem('10AM'));
 })
 $(document).ready(function(){
-  $('#11').val(localStorage.getItem('11AM'))
+  $('#11a').val(localStorage.getItem('11AM'))
   console.log(localStorage.getItem('11AM'));
 })
 $(document).ready(function(){
-  $('#12').val(localStorage.getItem('12PM'))
+  $('#12p').val(localStorage.getItem('12PM'))
   console.log(localStorage.getItem('12PM'));
 })
 $(document).ready(function(){
-  $('#1').val(localStorage.getItem('  1PM'))
+  $('#1p').val(localStorage.getItem('  1PM'))
   console.log(localStorage.getItem('  1PM'));
 })
 $(document).ready(function(){
-  $('#2').val(localStorage.getItem('  2PM'))
+  $('#2p').val(localStorage.getItem('  2PM'))
   console.log(localStorage.getItem('  2PM'));
 })
 $(document).ready(function(){
-  $('#3').val(localStorage.getItem('  3PM'))
+  $('#3p').val(localStorage.getItem('  3PM'))
   console.log(localStorage.getItem('  3PM'));
 })
 $(document).ready(function(){
-  $('#4').val(localStorage.getItem('  4PM'))
+  $('#4p').val(localStorage.getItem('  4PM'))
   console.log(localStorage.getItem('  4PM'));
 }) 
+
+document.getElementById('currentDay').innerText=moment().format('MMMM Do YYYY, h:mm:ss a');
+
+$(document).ready(function(){
+  for (i = 9; i <= 16; i++) {
+    var timeBlock = parseInt($('#'+i).attr('id'))
+    var currentTime = parseInt(moment().format('H'));
+  
+// var timeBlock = parseInt($('#10').text());
+// var timeBlock = parseInt($('#11').text());
+// var timeBlock = parseInt($('#12').text());
+// var timeBlock = parseInt($('#1').text());
+// var timeBlock = parseInt($('#2').text());
+// var timeBlock = parseInt($('#3').text());
+// var timeBlock = parseInt($('#4').text());
+console.log(timeBlock)
+console.log(currentTime)
+if (timeBlock < currentTime) {
+  // console.log('class = past')
+  $('#'+i).siblings('input').addClass('past')
+}
+else if (timeBlock > currentTime) {
+  // console.log('class = future')
+  $('#'+i).siblings('input').addClass('future')
+}
+else if (timeBlock = currentTime) {
+  // console.log('class = present')
+  $('#'+i).siblings('input').addClass('present')
+}
+}
+});
